fd57c50e4f7af520dd1b79583d729d5df6ea5778
Tom Metzger <metzgert@gmail.com>
2013-10-22 17:34:00 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,61 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022040906) do
+ActiveRecord::Schema.define(:version => 20131022211523) do
+
+  create_table "chromosomes", :force => true do |t|
+    t.string   "location"
+    t.integer  "gal4_line_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "collections", :force => true do |t|
+    t.string   "name"
+    t.integer  "gal4_line_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "expression_profiles", :force => true do |t|
+    t.integer  "gal4_line_id"
+    t.integer  "start_stage_id"
+    t.integer  "end_stage_id"
+    t.string   "assessed_by"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "expression_profiles_locations", :id => false, :force => true do |t|
+    t.integer "expression_profile_id"
+    t.integer "location_id"
+  end
+
+  create_table "gal4_lines", :force => true do |t|
+    t.string   "name"
+    t.integer  "version"
+    t.string   "p_element_backbone"
+    t.text     "regulatory_sequence"
+    t.string   "gene"
+    t.integer  "copynumber"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "stages", :force => true do |t|
+    t.string   "name"
+    t.string   "time_range"
+    t.integer  "order"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
