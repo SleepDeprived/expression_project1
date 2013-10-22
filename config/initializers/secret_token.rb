f4d35c8878dc8c2881776c9c90f6b0dc51eb9831
Tom Metzger <metzgert@gmail.com>
2013-10-22 00:02:53 -0400

    created new expression_app rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpressionApp::Application.config.secret_token = 'f675cee8637697bf1fed0a0dfac39e310f63ee487bfccef2c9a12b62b25117a55f95b6e5627c96f5f0399013755daeda06f8578760541c179a68688f3fb22c5a'
