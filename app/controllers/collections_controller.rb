10074f078cfacf348989b3690feaf92e64563a0c
Tom Metzger <metzgert@gmail.com>
2013-10-25 10:35:53 -0400

    fixed add collection redirect

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,9 +1,14 @@
 class CollectionsController < ApplicationController
 
   def create
-    @collection = Collection.new(params[:collection])
-    @user.collections << @collection
-    redirect_to("/dashboard")
+    if @user.nil?
+      flash[:notice] = "Please sign in to add the stock to your collection"
+      redirect_to('/login')
+    else
+      @collection = Collection.new(params[:collection])
+      @user.collections << @collection
+      redirect_to("/dashboard")
+    end
   end
 
 end
